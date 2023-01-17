0b58b8ac44da0423e32ffab942ae3184b1321d90
Xo <26947031+8C@users.noreply.github.com>
2023-01-17 15:02:59 -0700

    junk

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import time
+from pythonosc.udp_client import SimpleUDPClient
+
+battleMetricsServerID = 6324892 #get from url "https://www.battlemetrics.com/servers/rust/#######"
+
+IP, PORT = "127.0.0.1", 9000
+client = SimpleUDPClient(IP, PORT) 
+buffer = 3 * 60 #3min
+
+
+def getPop():
+
+    serverReq= requests.get(f'https://api.battlemetrics.com/servers/{battleMetricsServerID}').text
+    serverData = json.loads(serverReq)
+
+    if serverData == None:
+        return
+
+    serverPlayers = serverData['data']['attributes']['players']
+    serverMaxPlayers = serverData['data']['attributes']['maxPlayers']
+    serverQueue = serverData['data']['attributes']['details']['rust_queued_players']
+
+    if serverQueue > 0:
+        print(f"{serverPlayers}/{serverMaxPlayers} players / queue {serverQueue}")
+        client.send_message("/avatar/parameters/serverPlayers", serverPlayers)
+        client.send_message("/avatar/parameters/serverMaxPlayers", serverMaxPlayers)
+        client.send_message("/avatar/parameters/serverQueue", serverQueue)
+    else:
+        print(f"{serverPlayers}/{serverMaxPlayers} players / no queue")
+        client.send_message("/avatar/parameters/serverPlayers", serverPlayers)
+        client.send_message("/avatar/parameters/serverMaxPlayers", serverMaxPlayers)
+        client.send_message("/avatar/parameters/serverQueue", serverQueue)
+        
+    time.sleep(buffer)
+
+while True: #l0l
+    getPop()
